b79ba81780b24c1f7edb2b3f7b66c09bc3cce741
snOOpyCodesss <snoopy140224@gmail.com>
2024-06-03 13:54:02 +0530

    started implementing parallel routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-// import Link from "next/link";
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { error } from "console";
 import { desc } from "drizzle-orm";
 import Image from "next/image";
+import Link from "next/link";
 import { db } from "~/server/db";
 import { getMyImages } from "~/server/queries";
 
@@ -26,7 +26,7 @@ const images = await getMyImages();
     <div className="flex flex-wrap justify-center gap-4 p-4">
       {images.map((image) => (
         <div key={image.id} className="flex h-48 w-48 flex-col">
-          {/* <Link href={`/img/${image.id}`}> */}
+          <Link href={`/img/${image.id}`}>
             <Image
               src={image.url}
               style={{ objectFit: "contain" }}
@@ -34,7 +34,7 @@ const images = await getMyImages();
               height={192}
               alt={image.name}
             />
-          {/* </Link> */}
+          </Link>
           <div>{image.name}</div>
         </div>
       ))}
