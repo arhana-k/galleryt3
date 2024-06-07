3e0627823d551fbe74f88bf0d8c0853f106515f1
snOOpyCodesss <snoopy140224@gmail.com>
2024-06-07 22:21:01 +0530

    rate limit implemented

--- src/app/_components/simple-upload-button.tsx
+++ src/app/_components/simple-upload-button.tsx
@@ -88,11 +88,11 @@ export function SimpleUploadButton() {
         },
       );
     },
-//     onUploadError(error) {
-//       posthog.capture("upload_error", { error });
-//       toast.dismiss("upload-begin");
-//       toast.error("Upload failed");
-//     },
+    onUploadError(error) {
+      posthog.capture("upload_error", { error });
+      toast.dismiss("upload-begin");
+      toast.error("Upload failed");
+    },
     onClientUploadComplete() {
       toast.dismiss("upload-begin");
       toast("Upload complete!");
